c66b59bbb66f9a8813e3786e0f4ab83ca49d9c63
prateek saini <pratiiksaini@gmail.com>
2023-08-30 16:01:17 +0530

    updating the yml file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 echo -e $PWD
 
-echo -e '\e[1m\e[34mEntering into frontend directory...\e[0m\n'
+# echo -e '\e[1m\e[34mEntering into frontend directory...\e[0m\n'
 
-cd ~/compcloud
+# cd ~/compcloud
 
-echo -e $PWD
+# echo -e $PWD
 
-echo -e '\e[1m\e[34mPulling code from remote...\e[0m\n'
+# echo -e '\e[1m\e[34mPulling code from remote...\e[0m\n'
 
-git pull origin main
+# git pull origin main
 
-echo -e '\e[1m\e[34mAll done now!\e[0m\n'
+# echo -e '\e[1m\e[34mAll done now!\e[0m\n'
 
 echo -e '\e[1m\e[34mThanks for automating this deployment process!\e[0m\n'
