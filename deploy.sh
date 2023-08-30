583c502c09d7e6114623303c4a65ec904c2720b1
prateek saini <pratiiksaini@gmail.com>
2023-08-30 16:13:20 +0530

    updating action file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
+echo -e '\e[1m\e[34mEntering into frontend directory...\e[0m\n'
+
+cd ~/compcloud
+
 echo -e $PWD
 
-# echo -e '\e[1m\e[34mEntering into frontend directory...\e[0m\n'
+echo -e '\e[1m\e[34mPulling code from remote...\e[0m\n'
 
-# cd ~/compcloud
+git pull origin main
 
-# echo -e $PWD
+echo -e '\e[1m\e[34mAll done now!\e[0m\n'
 
-# echo -e '\e[1m\e[34mPulling code from remote...\e[0m\n'
-
-# git pull origin main
-
-# echo -e '\e[1m\e[34mAll done now!\e[0m\n'
+sudo mkdir -p dist
+sudo tar -xzf tmp/compcloud_build.tgz -C dist/ .
 
 echo -e '\e[1m\e[34mThanks for automating this deployment process!\e[0m\n'
